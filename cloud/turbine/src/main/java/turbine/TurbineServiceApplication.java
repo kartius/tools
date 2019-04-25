ff9cd74562b3b79430f16b4fb6b07f9e58d28647
nikolaytsyb <nikolay.tsyb@hys-enterprise.com>
2019-04-25 18:34:02 +0300

    add hystrix and turine implementation

---
+++ cloud/turbine/src/main/java/turbine/TurbineServiceApplication.java
@@ -0,0 +1,16 @@
+package turbine;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class TurbineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineServiceApplication.class, args);
+    }
+
+}
